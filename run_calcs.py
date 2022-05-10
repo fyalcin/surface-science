f47970eda0502591fbcf99f66d18f3097a09e889
firaty <firat.yalcin@univie.ac.at>
2022-05-10 10:00:13 +0200

    Some small changes.

--- run_calcs.py
+++ run_calcs.py
@@ -1,19 +1,14 @@
 import numpy as np
+from atomate.vasp.fireworks.core import StaticFW
+from fireworks import LaunchPad
+from fireworks.core.rocket_launcher import rapidfire
 from pymatgen.core.structure import Structure
 from scipy.spatial.transform import Rotation as R
-from atomate.vasp.workflows.presets.core import wf_bandstructure_no_opt
-from fireworks.core.rocket_launcher import rapidfire
-from fireworks import LaunchPad, Workflow, Firework
-
-from triboflow.firetasks.start_swfs import FT_StartSurfaceEnergySWF
-from triboflow.utils.database import Navigator
-from datetime import datetime
 
 lpad = LaunchPad.auto_load()
 
-
-
 rapidfire(lpad)
+
 
 def rotate_and_shift(vec, shift, angle):
     # angle = angle * np.pi / 180
@@ -42,9 +37,5 @@ for angle in np.arange(0, 50, 5):
         diff = rotate_and_shift(vec, shift, coeff * angle) - vec
         slab.translate_sites(indices=site, vector=diff, frac_coords=False, to_unit_cell=True)
     # slab.to('poscar', f"Si_recon_{angle}.vasp")
-    wf = wf_bandstructure_no_opt(slab)
+    wf = StaticFW(structure=slab, name=f"Si_{angle}_calc")
     lpad.add_wf(wf)
-
-
-
-
